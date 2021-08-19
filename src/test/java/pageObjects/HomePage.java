de590f9bc1116c36a0981c07f1997ecf44bc193a
Imran Ahmed <imsdhk@yahoo.com>
2021-08-18 22:13:59 -0400

    second commit

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,8 +1,13 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
 	
+	WebDriver driver;
 	
-	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
 
 }
